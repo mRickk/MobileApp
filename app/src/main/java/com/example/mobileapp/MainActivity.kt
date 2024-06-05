e26b9580d1aee11071cef00a560f8ebd319a2d2c
Riccardo Mazzi <riccardo.mazzi@studio.unibo.it>
2024-06-05 23:19:28 +0200

    Testing navigation

--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mobileapp.ui.theme.MobileAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +26,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavGraph(navController = navController)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+sealed class NavigationRoute(
+    val route: String
+) {
+    data object Diary : NavigationRoute("Diary")
+    data object Search : NavigationRoute("Search")
+    data object Profile : NavigationRoute("Profile")
+    data object Diet : NavigationRoute("Diet")
+    data object Overview : NavigationRoute("Overview")
+    data object AddFood : NavigationRoute("AddFood")
+    data object InsertFood : NavigationRoute("InsertFood")
+    data object AddExercise : NavigationRoute("AddExercise")
+    data object InsertExercise : NavigationRoute("InsertExercise")
+    data object AddRecipe : NavigationRoute("AddRecipe")
+    data object InsertRecipe : NavigationRoute("InsertRecipe")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobileAppTheme {
-        Greeting("Android")
+fun NavGraph(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavigationRoute.Diary.route,
+        modifier = modifier
+    ) {
+        composable(NavigationRoute.Diary.route) {
+
+        }
+        composable(NavigationRoute.Diary.route) {
+
+        }
     }
 }
